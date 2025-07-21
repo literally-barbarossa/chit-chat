d5f6107a6951d36b662758cc34d9f53745190f5e
literally-barbarossa <abhinavumangsingh@gmail.com>
2025-07-21 18:00:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const fs = require('fs')
+const app = express()
+const PORT = 3000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+let messages = []
+
+if (fs.existsSync('messages.json')) {
+  messages = JSON.parse(fs.readFileSync('messages.json'))
+}
+
+app.post('/send-message', (req, res) => {
+  const { username, color, message } = req.body
+  const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+
+  const newMsg = { username, color, message, timestamp }
+  messages.push(newMsg)
+
+  fs.writeFileSync('messages.json', JSON.stringify(messages, null, 2))
+
+  res.sendStatus(200)
+})
+
+app.get('/messages', (req, res) => {
+  res.json(messages)
+})
+
+app.listen(PORT, () => console.log(`ChitChat server live on http://localhost:${PORT}`))
